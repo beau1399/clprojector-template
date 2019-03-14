62fcd152ebcc84d827fbcf7b9aa375032a030e3d
dell <beau.wilkinson@gmail.com>
2019-03-13 21:11:50 -0400

    Better segregation of internal and draw files

--- resources/leiningen/new/clprojector_proj/src/cljs/clprojector/draw.cljs
+++ resources/leiningen/new/clprojector_proj/src/cljs/clprojector/draw.cljs
@@ -2,17 +2,6 @@
                       [{{name}}.util :as clu]
                       [{{name}}.internal :as int]))
 
-(def dist clu/camera-distance) 
-(def half-width (/ clu/virtual-width 2))
-(def half-height (/ clu/virtual-height 2))
-
-(defn project
-  ([x y z]
-   {:x (+ half-width (*  half-height (/ (* x dist) (+ dist z))))
-    :y (+ half-height (* half-height (/ (* y dist) (+ dist z))))})
-  ([{ :keys [x y z]}]
-   (project x y z)))
-
 (declare get-canvas)
 
 (defn make-canvas []
@@ -29,33 +18,9 @@
 
 (defn get-context [canvas] (.getContext canvas "2d" ))
 
-(defn ccw [x1 y1 x2 y2 x3 y3]
-  (let [valu (- (* (- y2 y1)(- x3 x2))
-                (* (- x2 x1)(- y3 y2)))](> valu 0)))
-
-(defn facet [ctx points r g b a]
-  (let [p1  (nth points 0)     p2  (nth points 1)     p3  (nth points 2)
-        pj1 (apply project p1) pj2 (apply project p2) pj3 (apply project p3)
-        x1  (:x pj1)           y1  (:y pj1)           x2  (:x pj2) y2 (:y pj2) x3 (:x pj3) y3 (:y pj3)]
-    (if (ccw x1 y1 x2 y2 x3 y3)     
-      (do
-        ;;;https://petewarden.com/2011/01/30/a-fundamental-bug-in-html5s-canvas/        
-        (set! (.-lineWidth ctx) (/ a 3)) ;seems to help seams!
-        (set! (.-fillStyle ctx)
-              (str "rgba(" r "," g "," b "," a ")"))        
-        (set! (.-strokeStyle ctx)
-        (str "rgba(" r  "," g "," b "," a ")"))
-        (.beginPath ctx)
-        (.moveTo ctx x1 y1)
-        (.lineTo ctx x2 y2)
-        (.lineTo ctx x3 y3)
-        (.closePath ctx)
-        (.fill ctx)        
-        (.stroke ctx)))))
-
 (defn facet-list [ctx l r g b a]
   (loop [ll l]
-    (apply facet           
+    (apply int/facet           
            (concat (conj (list (take 3 ll)) ctx)
                    (list r g b a)))
   (if (> (count ll) 3) (recur (drop 3 ll)))))
@@ -103,21 +68,21 @@
    (let [m (apply rotate-about-z (concat l (list theta)))]
     (list (:x m) (:y m) (:z m)))))
 
-(defn line [ctx x1 y1 z1 x2 y2 z2 r g b a ] ;3D
-   (let [twod1 (project x1 y1 z1  )
-         twod2 (project x2 y2 z2  )]
-     (int/line-2d ctx (:x twod1) (:y twod1) (:x twod2) (:y twod2) r g b a)))
-
 (defn make-pairs ([collect] (make-pairs collect []))
   ([collect product]
    (let [fragment (take 2 collect) pr (cons fragment product) ]
     (if (= (count fragment) 0) (rest product)
         (recur (rest collect) pr)))))
 
-;;Takes 3D coords
+
+;;;Pass-through w/ Z-clipping since funcs. in this file have that
+(defn line [ctx x1 y1 z1 x2 y2 z2 r g b a]
+ (if (and (> z1 -2)(> z2 -2))
+  (int/line ctx x1 y1 z1 x2 y2 z2 r g b a )))
+  
 (defn line-list [ctx r g b a l]
   (doseq [ elem (make-pairs l)]
-    (apply line           
+    (apply int/line           
            (concat (conj (concat (first elem)(second elem)) ctx)
                    (list r g b a)))))
 
@@ -128,9 +93,9 @@
       (set! (.-fillStyle ctx)  (str "rgba(" r "," g "," b "," a ")"))
       (set! (.-strokeStyle ctx)  (str "rgba(" r "," g "," b "," a ")"))
       (.beginPath ctx)
-      (.moveTo ctx (:x (apply project (first points)))(:y (apply project (first points))))
+      (.moveTo ctx (:x (apply int/project (first points)))(:y (apply int/project (first points))))
       (loop [p (drop 1 points)]
-        (.lineTo ctx (:x (apply project (first p)))(:y (apply project (first p))))
+        (.lineTo ctx (:x (apply int/project (first p)))(:y (apply int/project (first p))))
         (if (> (count p) 1)(recur (drop 1 p))))
       (.closePath ctx)
       (.fill ctx)
--- resources/leiningen/new/clprojector_proj/src/cljs/clprojector/internal.cljs
+++ resources/leiningen/new/clprojector_proj/src/cljs/clprojector/internal.cljs
@@ -1,4 +1,17 @@
-(ns {{name}}.internal)
+(ns {{name}}.internal
+    (:require
+     [{{name}}.util :as clu]))
+
+(def dist clu/camera-distance) 
+(def half-width (/ clu/virtual-width 2))
+(def half-height (/ clu/virtual-height 2))
+
+(defn project
+  ([x y z]
+   {:x (+ half-width (*  half-height (/ (* x dist) (+ dist z))))
+    :y (+ half-height (* half-height (/ (* y dist) (+ dist z))))})
+  ([{ :keys [x y z]}]
+   (project x y z)))
 
 (defn line-2d
   [ctx x1 y1 x2 y2 r g b a] ;2D
@@ -10,3 +23,32 @@
    (.lineTo ctx x2 y2)
    (.closePath ctx)
    (.stroke ctx))
+
+(defn line [ctx x1 y1 z1 x2 y2 z2 r g b a ]
+   (let [twod1 (project x1 y1 z1  )
+         twod2 (project x2 y2 z2  )]
+     (line-2d ctx (:x twod1) (:y twod1) (:x twod2) (:y twod2) r g b a)))
+
+(defn ccw [x1 y1 x2 y2 x3 y3]
+  (let [valu (- (* (- y2 y1)(- x3 x2))
+                (* (- x2 x1)(- y3 y2)))](> valu 0)))
+
+(defn facet [ctx points r g b a]
+  (let [p1  (nth points 0)     p2  (nth points 1)     p3  (nth points 2)
+        pj1 (apply project p1) pj2 (apply project p2) pj3 (apply project p3)
+        x1  (:x pj1)           y1  (:y pj1)           x2  (:x pj2) y2 (:y pj2) x3 (:x pj3) y3 (:y pj3)]
+    (if (ccw x1 y1 x2 y2 x3 y3)     
+      (do
+        ;;;https://petewarden.com/2011/01/30/a-fundamental-bug-in-html5s-canvas/        
+        (set! (.-lineWidth ctx) (/ a 3)) ;seems to help seams!
+        (set! (.-fillStyle ctx)
+              (str "rgba(" r "," g "," b "," a ")"))        
+        (set! (.-strokeStyle ctx)
+        (str "rgba(" r  "," g "," b "," a ")"))
+        (.beginPath ctx)
+        (.moveTo ctx x1 y1)
+        (.lineTo ctx x2 y2)
+        (.lineTo ctx x3 y3)
+        (.closePath ctx)
+        (.fill ctx)        
+        (.stroke ctx)))))
