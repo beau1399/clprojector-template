51828894e56e42b0d7cdc43451491ac170c3baf1
beau1399 <beau.wilkinson@gmail.com>
2019-03-20 20:47:38 -0400

    Z dimension starts at 0

--- resources/leiningen/new/clprojector_proj/src/cljs/clprojector/core.cljs
+++ resources/leiningen/new/clprojector_proj/src/cljs/clprojector/core.cljs
@@ -2,42 +2,45 @@
   (:require
    [{{name}}.draw :as cld]
    [reitit.frontend :as reitit]))
+
 (defn ^:export scene []
   (let[ctx (cld/get-context (cld/get-canvas))]
    (def angle (atom 0))    
-    (def place (atom -0.5))
+    (def place (atom 0))
     (js/setInterval     
      (fn []
        (cld/cls ctx 0 0 0)
 ;;;Background lines
        (dorun (map #(cld/line ctx
-                              (- (* % 0.25) 2.5) -1 0
-                              (- (* % 0.25) 2.5) 1 0
+                              (- (* % 0.25) 2.5) -1 2
+                              (- (* % 0.25) 2.5) 1 2
                               255 0 0 1)(range 20)))
 ;;;Orbiting cube
        (cld/line-list
         ctx 255 0 255 1
         (map
-         #(cld/rotate-about-y % @angle)
+         #(cld/translate % 0 0 2)        
          (map
-          #(cld/translate % 1 0 0)
-          (list
-           '(-0.25 -0.25 -0.25)
-           '(0.25 -0.25 -0.25)
-           '(0.25 -0.25 0.25)
-           '(-0.25 -0.25 0.25)
-           '(-0.25 -0.25 -0.25)
-           '(-0.25 0.25 -0.25)
-           '(0.25 0.25 -0.25)
-           '(0.25 0.25 0.25)
-           '(-0.25 0.25 0.25)
-           '(-0.25 0.25 -0.25)
-           '(-0.25 0.25 0.25)
-           '(-0.25 -0.25 0.25)
-           '(0.25 -0.25 0.25)
-           '(0.25 0.25 0.25)
-           '(0.25 0.25 -0.25)
-           '(0.25 -0.25 -0.25)))))    
+          #(cld/rotate-about-y % @angle)
+          (map
+           #(cld/translate % 1 0 0)
+           (list
+            '(-0.25 -0.25 -0.25)
+            '(0.25 -0.25 -0.25)
+            '(0.25 -0.25 0.25)
+            '(-0.25 -0.25 0.25)
+            '(-0.25 -0.25 -0.25)
+            '(-0.25 0.25 -0.25)
+            '(0.25 0.25 -0.25)
+            '(0.25 0.25 0.25)
+            '(-0.25 0.25 0.25)
+            '(-0.25 0.25 -0.25)
+            '(-0.25 0.25 0.25)
+            '(-0.25 -0.25 0.25)
+            '(0.25 -0.25 0.25)
+            '(0.25 0.25 0.25)
+            '(0.25 0.25 -0.25)
+            '(0.25 -0.25 -0.25))))))
 ;;;Travelling Cube (most of it)
        (cld/facet-list
         ctx  
@@ -105,6 +108,7 @@
        (cld/write-text ctx 0.025 0.05 0 255 0 1 22 "CLProjector Demo" "monospace")
        (cld/draw-image ctx 0.85 0.825 "img1")
        
-       (swap! place #(if (< % 100) (+ 0.05 %) -0.5))
+       (swap! place #(if (< % 100) (+ 0.05 %) 0))
        (swap! angle #(+ 0.01 %))
        ) 10)))
+
