8dfe627a07790e8ee96228102df441b1c226e6a2
beau1399 <beau.wilkinson@gmail.com>
2019-03-21 20:25:23 -0400

    Projection correction

--- resources/leiningen/new/clprojector_proj/src/cljs/clprojector/internal.cljs
+++ resources/leiningen/new/clprojector_proj/src/cljs/clprojector/internal.cljs
@@ -16,8 +16,8 @@
    (let [zprime (- pz dist)]
     (loop [x px y py z zprime]  
      (if (> z (- 0 dist)) ;Don't return something behind the camera...
-      {:x (+ (half-width) (*  (half-height) (/ (* x dist) (+ dist z))))
-       :y (+ (half-height) (* (half-height) (/ (* y dist) (+ dist z))))}
+      {:x (+ (half-width) (*  (half-height) (/  x (+ dist z))))
+       :y (+ (half-height) (* (half-height) (/  y (+ dist z))))}
       (recur (* 0.9 x)(* 0.9 y)(* 0.9 z)) ;...retract to camera plane instead
    ))))
   ([{ :keys [x y z]}]
